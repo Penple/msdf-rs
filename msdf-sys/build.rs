cdd4f3e90ab65d3eee7c1499bf69db9320bed48d
Gwen Lofman <git@glfmn.io>
2022-10-10 17:55:13 -0400

    Generate bindings file once for faster compilation
    This reduces the clean compile time (on my laptop) from 25s to 8s
    and prunes 29 dependencies from the dependency tree.  However, this
    only includes bindings for x86_64-linux-gnu as I have not set this
    machine up for cross compiling.

--- msdf-sys/build.rs
+++ msdf-sys/build.rs
@@ -1,9 +1,9 @@
 use cmake::Config;
-use std::{env, fs};
-use std::fs::{OpenOptions, remove_dir_all};
-use std::io::Write;
-use std::path::PathBuf;
 use fs_extra::dir::{copy, CopyOptions};
+use std::fs::{remove_dir_all, OpenOptions};
+use std::io::Write;
+use std::path::{Path, PathBuf};
+use std::{env, fs};
 
 fn main() {
     let out = PathBuf::from(env::var("OUT_DIR").unwrap());
@@ -54,19 +54,57 @@ fn main() {
         println!("cargo:rustc-link-lib=dylib=stdc++");
     }
 
-    let bindings = bindgen::Builder::default()
-        .clang_arg("-Imsdfgen")
-        .clang_arg("-x")
-        .clang_arg("c++")
-        .opaque_type("std::.*")
-        .allowlist_type("msdfgen::.*")
-        .allowlist_function("msdfgen::.*")
-        .header("wrapper.h")
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-        .generate()
-        .expect("Unable to generate bindings");
+    fn bindings_filename() -> String {
+        format!(
+            "{}-{}-{}.rs",
+            env::var("CARGO_CFG_TARGET_ARCH").unwrap(),
+            env::var("CARGO_CFG_TARGET_OS").unwrap(),
+            env::var("CARGO_CFG_TARGET_ENV").unwrap()
+        )
+    }
 
-    bindings
-        .write_to_file(out.join("bindings.rs"))
-        .expect("Couldn't write bindings!");
+    #[cfg(any(not(feature = "bindgen"), feature = "update-bindings"))]
+    fn bindings_filepath(filename: &str) -> impl AsRef<Path> {
+        Path::new("src").join("bindings").join(filename)
+    }
+
+    #[cfg(not(feature = "bindgen"))]
+    {
+        let bindings_file = bindings_filename();
+
+        if bindings_filepath(&bindings_file).as_ref().is_file() {
+            println!("cargo:rustc-env=MSDF_BINDINGS={}", bindings_file);
+        } else {
+            panic!("No prebuilt bindings. Try use `bindgen` feature.",);
+        }
+    }
+
+    #[cfg(feature = "bindgen")]
+    {
+        let bindings = bindgen::Builder::default()
+            .clang_arg("-Imsdfgen")
+            .clang_arg("-x")
+            .clang_arg("c++")
+            .opaque_type("std::.*")
+            .allowlist_type("msdfgen::.*")
+            .allowlist_function("msdfgen::.*")
+            .header("wrapper.h")
+            .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+            .generate()
+            .expect("Unable to generate bindings");
+
+        let bind_file = out.join("bindings.rs");
+        bindings
+            .write_to_file(&bind_file)
+            .expect("Couldn't write bindings!");
+
+        #[cfg(feature = "update-bindings")]
+        //(bind_file: impl AsRef<Path>, dest_file: impl AsRef<Path>)
+        {
+            let dest_file = bindings_filepath(&bindings_filename());
+
+            fs::create_dir_all(&dest_file.as_ref().parent().unwrap()).unwrap();
+            fs::copy(&bind_file, &dest_file).unwrap();
+        }
+    }
 }
